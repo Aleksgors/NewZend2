2999e61e9b6a775f6a28ef4147d8e9ebdc7256bb
Alexander Gorshkov <gorshkov.duk@yandex.ru>
2020-03-02 08:49:38 +0300

    Working

---
+++ module/Admin/config/module.config.php
@@ -0,0 +1,117 @@
+<?php
+
+return array(
+
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController',
+            'category' => 'Admin\Controller\CategoryController',
+            'article' => 'Admin\Controller\ArticleController',
+        ),
+    ),
+
+    'router' => array(
+        'routes' => array(
+            'admin' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/admin[/]',
+                    'defaults' => array(
+                        'controller' => 'Admin\Controller\Index',
+                        'action' => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+
+                'child_routes' => array(
+                    'category' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route' => 'category[/][:action][/:id/]',
+                            'defaults' => array(
+                                'controller' => 'category',
+                                'action' => 'index',
+                            ),
+                        ),
+                    ),
+                    'article' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route' => 'article[/][:action/][:id/]',
+                            'defaults' => array(
+                                'controller' => 'article',
+                                'action' => 'index',
+                            ),
+                        ),
+                    ),
+                ), //< child_routes
+            ),
+        ),
+    ),
+
+    'service_manager' => array(
+        'factories' => array(
+            'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+            'admin_navigation' => 'Admin\Lib\AdminNavigationFactory',
+        ),
+    ),
+
+    'navigation' => array(
+        'default' => array(
+            array(
+                'label' => 'Главная',
+                'route' => 'home',
+            ),
+        ),
+
+        'admin_navigation' => array(
+            array(
+                'label' => 'Панель управления сайтом',
+                'route' => 'admin',
+                'action' => 'index',
+                'resource' => 'Admin\Controller\Index',
+
+                'pages' => array(
+                    array(
+                        'label' => 'Статьи',
+                        'route' => 'admin/article',
+                        'action' => 'index',
+                        'class' => 'nav-link',
+                    ),
+                    array(
+                        'label' => 'Добавить статью',
+                        'route' => 'admin/article',
+                        'action' => 'add',
+                        'class' => 'nav-link',
+                    ),
+                    array(
+                        'label' => 'Категории',
+                        'route' => 'admin/category',
+                        'action' => 'index',
+                        'class' => 'nav-link',
+                    ),
+                    array(
+                        'label' => 'Добавить категорию',
+                        'route' => 'admin/category',
+                        'action' => 'add',
+                        'class' => 'nav-link',
+                    ),
+    //                array(
+    //                    'label' => 'Комментарии',
+    //                    'route' => 'admin/comment',
+    //                    'action' => 'index',
+    //                ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+        'template_map' => array(
+            'pagination_control' => __DIR__ . '/../view/layout/pagination_control.phtml',
+        ),
+    ),
+);
---
+++ module/Admin/src/Admin/Controller/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class IndexController extends AbstractActionController
+{
+    public function IndexAction()
+    {
+
+    }
+}
